b047d381fd18189edb8ac413bf4f3f21ca33176b
delafosm <mathis.delafosse@gmail.com>
2021-04-02 11:30:21 +0200

    added login

--- app/src/main/java/com/example/projet/MainActivity.java
+++ app/src/main/java/com/example/projet/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.projet.db.DatabaseClient;
@@ -25,10 +27,15 @@ public class MainActivity extends AppCompatActivity {
         // Récupération du DatabaseClient
         mDb = DatabaseClient.getInstance(getApplicationContext());
 
-        getStudents();
+        Button boutonConnexion = findViewById(R.id.main_BoutonConnexion);
+        boutonConnexion.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                checkStudent();
+            }
+        });
     }
 
-    public void onChoix(View view) {
+    public void onBoutonAnonyme(View view) {
         // Création d'une intention
         Intent ChoixActivity = new Intent(this, ChoixActivity.class);
 
@@ -36,8 +43,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(ChoixActivity);
     }
 
-    private void getStudents() {
-        ///////////////////////
+    private void checkStudent() {
         // Classe asynchrone permettant de récupérer des taches et de mettre à jour le listView de l'activité
         class getStudents extends AsyncTask<Void, Void, List<Student>> {
 
@@ -52,13 +58,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(List<Student> students) {
                 super.onPostExecute(students);
-
-                // Message
-                Toast.makeText(MainActivity.this, "LongClick : " + students.get(0).getFirstName(), Toast.LENGTH_LONG).show();
+                checkIfStudentExist(students);
             }
         }
 
         getStudents gs = new getStudents();
         gs.execute();
+    }
+
+    private void checkIfStudentExist(List<Student> students){
+        EditText surNameEdit = findViewById(R.id.main_ChampNom);
+        String surName = surNameEdit.getText().toString();
+        EditText firstNameEdit = findViewById(R.id.main_ChampPrenom);
+        String firstName = firstNameEdit.getText().toString();
+        boolean studentFound = false;
+
+        for(Student student : students){
+            if(student.getFirstName().equalsIgnoreCase(firstName)){
+                studentFound = true;
+                // Création d'une intention
+                Intent ChoixActivity = new Intent(this, ChoixActivity.class);
+
+                // Lancement de la demande de changement d'activité
+                startActivity(ChoixActivity);
+            }
+        }
+        if(!studentFound){
+            Toast.makeText(MainActivity.this, "Identifiants incorrects", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
